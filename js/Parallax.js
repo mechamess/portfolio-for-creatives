3105e3438fbc2ae50185e4d8df7cee811f2f0921
Emma Do <emmado999@gmail.com>
2024-02-28 23:29:54 -0800

    para

--- js/Parallax.js
+++ js/Parallax.js
@@ -1,7 +1,19 @@
-$(document).ready(function() {
-  $('.experiment-image-1').click(function() {
-    $('.experiment-image-2').fadeToggle("slow");
-    // Alternative animation for example
-    // slideToggle("fast");
-  });
-});
+(function() {
+  // Add event listener
+  document.addEventListener("mousemove", parallax);
+  const elem = document.querySelector("#parallax");
+  // Magic happens here
+  function parallax(e) {
+      let _w = window.innerWidth/2;
+      let _h = window.innerHeight/2;
+      let _mouseX = e.clientX;
+      let _mouseY = e.clientY;
+      let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+      let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+      let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
+      let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+      console.log(x);
+      elem.style.backgroundPosition = x;
+  }
+
+})();
